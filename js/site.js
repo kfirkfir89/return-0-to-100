81a362a355947dd3732384c0678a0bd2bd400004
kfir <kfirkfir89@gmail.com>
2022-01-30 19:38:14 +0200

    finish the most of challenge
    finish all the js need to add for links

--- js/site.js
+++ js/site.js
@@ -1,3 +1,63 @@
-function helloWorld(){
-    alert("hello world");
+//get the values from the Page
+//starts or controller function
+function getValues(){
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //we need validate our input
+    //parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)){
+        //we call generateNumbers
+        let numbers = generateNumbers(startValue, endValue); 
+        //we call displayNumbers
+        displayNumbers(numbers);
+    }
+    else{
+        alert("You must enter integers");
+    }
+}
+
+
+//generate numbers from startvalue to endvalue
+//logic function(s)
+function generateNumbers(sValue, eValue){
+
+    let numbers = [];
+
+    //we want to get all numbers from start to end
+    for (let index = sValue; index <= eValue ; index++){
+
+        //this will execute in a loop until index = evalue
+        numbers.push(index);
+    }
+
+    return numbers;
+
+}
+
+
+//display the numbers and mark even numbers bold
+//display or view functions
+function displayNumbers(numbers){
+
+    let templateRows = "";
+    for (let index = 0; index < numbers.length; index++){
+
+        let className = "even";
+        let number = numbers[index];
+
+        if(number % 2 == 0){
+            className = "even";
+        }else{
+            className = "odd";
+        }
+
+        templateRows += `<tr><td class="${className}">${number}</td><tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
 }
